1a961ee60d59f39215bc8622962a7ad9ae2c217b
grosi <grosi.mail@gmail.com>
2014-02-18 09:08:57 +0100

    algorithm for min. spanning tree works, but is not efficent!

--- main.m
+++ main.m
@@ -13,11 +13,13 @@ clear all;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % graphic settings %
 MARKER_LINE_WIDHT = 2;
+LINE_LINE_WIDTH = 3;
 MARKER_SIZE = 20;
 MARKER_NORMAL_COLORSHAPE = 'ro';
 MARKER_NORMAL_COLOR = 'r';
 MARKER_START_COLOR = 'g';
 MARKER_CLUSTER_COLOR = 'b';
+LINE_TREE_COLOR = 'g';
 
 
 % play ground %
@@ -159,70 +161,66 @@ node_edge_to_node = zeros(nodes_quantity,nodes_quantity);
 node_edge_weight = zeros(nodes_quantity,nodes_quantity);
 for i = 1:nodes_quantity
     for j = 1:nodes_quantity
-        
-        if j > i
         dx = abs(nodes_x_positions(j)-nodes_x_positions(i));
         dy = abs(nodes_y_positions(j)-nodes_y_positions(i));
         
-        %if dx > 0
+        if dx > 0 || dy > 0
             node_edge_to_node(i,j) = j;
             node_edge_weight(i,j) = sqrt(dx^2 + dy^2);
             plot([nodes_x_positions(i) nodes_x_positions(j)],...
                 [nodes_y_positions(i) nodes_y_positions(j)]);
-        %end
         end
     end
 end
 
-%% 6. Prim
-node_queue = zeros(nodes_quantity,1);
+%% 6. Prim algortihm -> min. spanning tree
 T_nodes = zeros(nodes_quantity,1);
 T_nodes(1) = start_node;
 node_in_T = 1;
+tmp_node_edge_weight = node_edge_weight;
+tmp_node_edge_weight(:,start_node) = 0; % start node is still connected to tree
+tree_node_connections = zeros(nodes_quantity,nodes_quantity);
 
 %find minimum spanning tree
 while node_in_T < nodes_quantity
+    %loop througt the T-nodes (nodes in tree)
+    tmp_edge_weight = 1000000;
     for i= 1:node_in_T
 
-        tmp_edge_weight = 1000000;
+        
         for j= 1:nodes_quantity
-            if 0 < node_edge_weight(T_nodes(i),j)
-                if tmp_edge_weight > node_edge_weight(T_nodes(i),j)
-                    tmp_edge_weight = node_edge_weight(T_nodes(i),j);
-                    %node_edge_weight(T_nodes(i),j) = 0;
-                    T_node = T_nodes(i);
-                    T_nodes(node_in_T+1) = j;
+            if T_nodes(i) ~= j && 0 < tmp_node_edge_weight(T_nodes(i),j)
+            %if 0 < tmp_node_edge_weight(T_nodes(i),j)
+                if tmp_edge_weight > tmp_node_edge_weight(T_nodes(i),j)
+                    
+                    if max(tree_node_connections(:,j)) == 0
+                        % way is done -> delete
+                        tmp_edge_weight = tmp_node_edge_weight(T_nodes(i),j);
+    %                     tmp_node_edge_weight(T_nodes(i),j) = 0;
+    %                     tmp_node_edge_weight(j,T_nodes(i)) = 0;
 
+                        % save the two nodes with smallest edge-weight (temporary)
+                        T_node = T_nodes(i);
+                        T_nodes(node_in_T+1) = j;
+                    end
                 end
             end
-        end
-
-        
+        end   
     end
+    
+    %one node added to tree
     node_in_T = node_in_T + 1;
-    plot([nodes_x_positions(T_nodes(T_node)) nodes_x_positions(T_nodes(node_in_T))],...
-                [nodes_y_positions(T_nodes(T_node)) nodes_y_positions(T_nodes(node_in_T))],'g','LineWidth',2);
+    
+    %history about the tree connections
+    tmp_node_edge_weight(T_node,T_nodes(node_in_T)) = 0;
+    tmp_node_edge_weight(T_nodes(node_in_T),T_node) = 0;
+    tree_node_connections(T_node,T_nodes(node_in_T)) = node_edge_weight(T_node,T_nodes(node_in_T));
+    
+    plot([nodes_x_positions(T_node) nodes_x_positions(T_nodes(node_in_T))],...
+                [nodes_y_positions(T_node) nodes_y_positions(T_nodes(node_in_T))],LINE_TREE_COLOR,'LineWidth',LINE_LINE_WIDTH);
 end
 
 
-%%
-% node_edge_weight_minimum = 0;
-% node_edge_complete_weight = 0;
-% 
-% for i= 1:nodes_quantity
-%     for j = 1:nodes_quantity
-%         if  node_edge_weight(i,j) > 0
-%             if node_edge_weight_minimum > node_edge_weight(i,j)
-%                 node_edge_weight_minimum = node_edge_weight(i,j);
-%                 
-%             else
-%                 if node_edge_weight_minimum == node_edge_weight(i,j)
-%                     node_edge_complete_weight = node_edge_complete_weight + node_edge_weight(i,j);
-%                 end
-%             end
-%         end
-%     end
-% end
 
 
 
