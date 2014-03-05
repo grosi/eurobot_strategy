e5c1d6feab21852336adb389f70f6618cd2f88b9
grosi <grosi.mail@gmail.com>
2014-03-05 20:33:09 +0100

    emprovment weight-function

--- main.m
+++ main.m
@@ -41,14 +41,15 @@ PLAYGROUND_HEIGHT = 2; % height in meter
 % strategy settings %
 STRATEGY_TRACK_ENEMY_GRID_SIZE_X = 10e-2; %10cm
 STRATEGY_TRACK_ENEMY_GRID_SIZE_Y = 10e-2; %10cm
-STRATEGY_TRACK_CENTER_WEIGHT = 2;
-STRATEGY_TRACK_FRAME_WEIGHT = 1;
+STRATEGY_TRACK_CENTER_WEIGHT = 1;
+STRATEGY_TRACK_FRAME_WEIGHT = 0.5;
 STRATEGY_TRACK_TRESHHOLD = 5;
 
 % node settings %
 NODE_QUANTITY = 11;
 
 % ball
+NODE_BALL_ID = 1;
 NODE_BALL_TIME = 5; %[s]
 NODE_BALL_POINT = 4; %max. possible points;
 NODE_BALL_PERCENT_OF_POINTS = 3*NODE_BALL_POINT/TOTAL_POINTS/3;
@@ -66,6 +67,7 @@ NODE_BALL_3_2_X_POSITION = 2.4; % [m]
 NODE_BALL_3_2_Y_POSITION = 0.49; % [m]
 
 % start
+NODE_START_ID = 2;
 NODE_START_TIME = 1;
 NODE_START_POINT = 1;
 NODE_START_PERCENT_OF_POINTS = 1*NODE_START_POINT/TOTAL_POINTS/1;
@@ -73,6 +75,7 @@ NODE_START_X_POSITION = 0.25; % [m]
 NODE_START_Y_POSITION = 0.25; % [m]
 
 % fresco
+NODE_FRESCO_ID = 3;
 NODE_FRESCO_TIME = 10; %[s]
 NODE_FRESCO_POINT = 6;
 NODE_FRESCO_PERCENT_OF_POINTS = 1*NODE_FRESCO_POINT/TOTAL_POINTS/1;
@@ -82,6 +85,7 @@ NODE_FRESCO_1_2_X_POSITION = 1.65; % [m]
 NODE_FRESCO_1_2_Y_POSITION = 0.2; % [m]
 
 % fire
+NODE_FIRE_ID = 4;
 NODE_FIRE_TIME = 2; %[s]
 NODE_FIRE_POINT = 1;
 NODE_FIRE_PERCENT_OF_POINTS = 3*NODE_FIRE_POINT/TOTAL_POINTS/3;
@@ -105,17 +109,13 @@ title('Gegener Position');
 hold on;
 % node-weight development
 NODEWEIGHT = figure();
-subplot(221);
+subplot(211);
 xlim([0 PLAY_TIME]);
 title('Knotengewichte');
 hold on;
-subplot(222);
+subplot(212);
 xlim([0 PLAY_TIME]);
 title('Gewicht-Gewicht');
-hold on;
-subplot(223);
-xlim([0 PLAY_TIME]);
-title('Empty');
 hold on;
 % playground
 PLAYAREA = figure();
@@ -130,116 +130,139 @@ nodes_quantity = NODE_QUANTITY;
 
 %% 3. set the nodes
 % ball 1
+nodes(1).id = NODE_BALL_ID;
 nodes(1).points = NODE_BALL_POINT;
 nodes(1).percent = NODE_BALL_PERCENT_OF_POINTS;
 nodes(1).time = NODE_BALL_TIME;
 nodes(1).x = (NODE_BALL_1_1_X_POSITION*PLAYGROUND_IMAGE_WIDTH)/PLAYGROUND_WIDTH;
 nodes(1).y = (NODE_BALL_1_1_Y_POSITION*PLAYGROUND_IMAGE_WIDTH)/PLAYGROUND_WIDTH;
 nodes(1).weight = 0;
+nodes(1).weighttext = text(nodes(1).x-MARKER_SIZE,nodes(1).y+MARKER_SIZE+MARKER_LINE_WIDHT,'weight','Color','m');
 nodes(1).cluster = 0; %number of the other node
 nodes(1).child = 0;
 
 % ball 2
+nodes(2).id = NODE_BALL_ID;
 nodes(2).points = NODE_BALL_POINT;
 nodes(2).percent = NODE_BALL_PERCENT_OF_POINTS;
 nodes(2).time = NODE_BALL_TIME;
 nodes(2).x = (NODE_BALL_1_2_X_POSITION*PLAYGROUND_IMAGE_WIDTH)/PLAYGROUND_WIDTH;
 nodes(2).y = (NODE_BALL_1_2_Y_POSITION*PLAYGROUND_IMAGE_WIDTH)/PLAYGROUND_WIDTH;
 nodes(2).weight = 0;
+nodes(2).weighttext = text(nodes(2).x-MARKER_SIZE,nodes(2).y+MARKER_SIZE+MARKER_LINE_WIDHT,'weight','Color','m');
 nodes(2).cluster = 0; %number of the other node
 nodes(2).child = 0;
 
 % ball 3
+nodes(3).id = NODE_BALL_ID;
 nodes(3).points = NODE_BALL_POINT;
 nodes(3).percent = NODE_BALL_PERCENT_OF_POINTS;
 nodes(3).time = NODE_BALL_TIME;
 nodes(3).x = (NODE_BALL_2_1_X_POSITION*PLAYGROUND_IMAGE_WIDTH)/PLAYGROUND_WIDTH;
 nodes(3).y = (NODE_BALL_2_1_Y_POSITION*PLAYGROUND_IMAGE_WIDTH)/PLAYGROUND_WIDTH;
 nodes(3).weight = 0;
+nodes(3).weighttext = text(nodes(3).x-MARKER_SIZE,nodes(3).y+MARKER_SIZE+MARKER_LINE_WIDHT,'weight','Color','m');
 nodes(3).cluster = 0; %number of the other node
 nodes(3).child = 0;
 
 % ball 4
+nodes(4).id = NODE_BALL_ID;
 nodes(4).points = NODE_BALL_POINT;
 nodes(4).percent = NODE_BALL_PERCENT_OF_POINTS;
 nodes(4).time = NODE_BALL_TIME;
 nodes(4).x = (NODE_BALL_2_2_X_POSITION*PLAYGROUND_IMAGE_WIDTH)/PLAYGROUND_WIDTH;
 nodes(4).y = (NODE_BALL_2_2_Y_POSITION*PLAYGROUND_IMAGE_WIDTH)/PLAYGROUND_WIDTH;
 nodes(4).weight = 0;
+nodes(4).weighttext = text(nodes(4).x-MARKER_SIZE,nodes(4).y+MARKER_SIZE+MARKER_LINE_WIDHT,'weight','Color','m');
 nodes(4).cluster = 0; %number of the other node
 nodes(4).child = 0;
 
 % ball 5
+nodes(5).id = NODE_BALL_ID;
 nodes(5).points = NODE_BALL_POINT;
 nodes(5).percent = NODE_BALL_PERCENT_OF_POINTS;
 nodes(5).time = NODE_BALL_TIME;
 nodes(5).x = (NODE_BALL_3_1_X_POSITION*PLAYGROUND_IMAGE_WIDTH)/PLAYGROUND_WIDTH;
 nodes(5).y = (NODE_BALL_3_1_Y_POSITION*PLAYGROUND_IMAGE_WIDTH)/PLAYGROUND_WIDTH;
 nodes(5).weight = 0;
+nodes(5).weighttext = text(nodes(5).x-MARKER_SIZE,nodes(5).y+MARKER_SIZE+MARKER_LINE_WIDHT,'weight','Color','m');
 nodes(5).cluster = 0; %number of the other node
 nodes(5).child = 0;
 
 % ball 6
+nodes(6).id = NODE_BALL_ID;
 nodes(6).points = NODE_BALL_POINT;
 nodes(6).percent = NODE_BALL_PERCENT_OF_POINTS;
 nodes(6).time = NODE_BALL_TIME;
 nodes(6).x = (NODE_BALL_3_2_X_POSITION*PLAYGROUND_IMAGE_WIDTH)/PLAYGROUND_WIDTH;
 nodes(6).y = (NODE_BALL_3_2_Y_POSITION*PLAYGROUND_IMAGE_WIDTH)/PLAYGROUND_WIDTH;
 nodes(6).weight = 0;
+nodes(6).weighttext = text(nodes(6).x-MARKER_SIZE,nodes(6).y+MARKER_SIZE+MARKER_LINE_WIDHT,'weight','Color','m');
 nodes(6).cluster = 0; %number of the other node
 nodes(6).child = 0;
 
 % fresco 1
+nodes(7).id = NODE_FRESCO_ID;
 nodes(7).points = NODE_FRESCO_POINT;
 nodes(7).percent = NODE_FRESCO_PERCENT_OF_POINTS;
 nodes(7).time = NODE_FRESCO_TIME;
 nodes(7).x = (NODE_FRESCO_1_1_X_POSITION*PLAYGROUND_IMAGE_WIDTH)/PLAYGROUND_WIDTH;
 nodes(7).y = (NODE_FRESCO_1_1_Y_POSITION*PLAYGROUND_IMAGE_WIDTH)/PLAYGROUND_WIDTH;
 nodes(7).weight = 0;
+nodes(7).weighttext = text(nodes(7).x-MARKER_SIZE,nodes(7).y+MARKER_SIZE+MARKER_LINE_WIDHT,'weight','Color','m');
 nodes(7).cluster = 0; %number of the other node
 nodes(7).child = 0;
 
 % fresco 2
+nodes(8).id = NODE_FRESCO_ID;
 nodes(8).points = NODE_FRESCO_POINT;
 nodes(8).percent = NODE_FRESCO_PERCENT_OF_POINTS;
 nodes(8).time = NODE_FRESCO_TIME;
 nodes(8).x = (NODE_FRESCO_1_2_X_POSITION*PLAYGROUND_IMAGE_WIDTH)/PLAYGROUND_WIDTH;
 nodes(8).y = (NODE_FRESCO_1_2_Y_POSITION*PLAYGROUND_IMAGE_WIDTH)/PLAYGROUND_WIDTH;
 nodes(8).weight = 0;
+nodes(8).weighttext = text(nodes(8).x-MARKER_SIZE,nodes(8).y+MARKER_SIZE+MARKER_LINE_WIDHT,'weight','Color','m');
 nodes(8).cluster = 0; %number of the other node
 nodes(8).child = 0;
 
 % fire 1
+nodes(9).id = NODE_FIRE_ID;
 nodes(9).points = NODE_FIRE_POINT;
 nodes(9).percent = NODE_FIRE_PERCENT_OF_POINTS;
 nodes(9).time = NODE_FIRE_TIME;
 nodes(9).x = (NODE_FIRE_1_X_POSITION*PLAYGROUND_IMAGE_WIDTH)/PLAYGROUND_WIDTH;
 nodes(9).y = (NODE_FIRE_1_Y_POSITION*PLAYGROUND_IMAGE_WIDTH)/PLAYGROUND_WIDTH;
 nodes(9).weight = 0;
+nodes(9).weighttext = text(nodes(9).x-MARKER_SIZE,nodes(9).y+MARKER_SIZE+MARKER_LINE_WIDHT,'weight','Color','m');
 nodes(9).cluster = 0; %number of the other node
 nodes(9).child = 0;
 
 % fire 2
+nodes(10).id = NODE_FIRE_ID;
 nodes(10).points = NODE_FIRE_POINT;
 nodes(10).percent = NODE_FIRE_PERCENT_OF_POINTS;
 nodes(10).time = NODE_FIRE_TIME;
 nodes(10).x = (NODE_FIRE_2_X_POSITION*PLAYGROUND_IMAGE_WIDTH)/PLAYGROUND_WIDTH;
 nodes(10).y = (NODE_FIRE_2_Y_POSITION*PLAYGROUND_IMAGE_WIDTH)/PLAYGROUND_WIDTH;
 nodes(10).weight = 0;
+nodes(10).weighttext = text(nodes(10).x-MARKER_SIZE,nodes(10).y+MARKER_SIZE+MARKER_LINE_WIDHT,'weight','Color','m');
 nodes(10).cluster = 0; %number of the other node
 nodes(10).child = 0;
 
 % fire 3
+nodes(11).id = NODE_FIRE_ID;
 nodes(11).points = NODE_FIRE_POINT;
 nodes(11).percent = NODE_FIRE_PERCENT_OF_POINTS;
 nodes(11).time = NODE_FIRE_TIME;
 nodes(11).x = (NODE_FIRE_3_X_POSITION*PLAYGROUND_IMAGE_WIDTH)/PLAYGROUND_WIDTH;
 nodes(11).y = (NODE_FIRE_3_Y_POSITION*PLAYGROUND_IMAGE_WIDTH)/PLAYGROUND_WIDTH;
 nodes(11).weight = 0;
+nodes(11).weighttext = text(nodes(11).x-MARKER_SIZE,nodes(11).y+MARKER_SIZE+MARKER_LINE_WIDHT,'weight','Color','m');
 nodes(11).cluster = 0; %number of the other node
 nodes(11).child = 0;
 
 % start
+% nodes(12).id = NODE_START_ID;
 % nodes(12).points = NODE_START_POINT;
 % nodes(12).percent = NODE_START_PERCENT_OF_POINTS;
 % nodes(12).time = NODE_START_TIME;
@@ -258,7 +281,6 @@ for i = 1:nodes_quantity
     tmp_child_data = get(gca,'Children');
     nodes(i).child = tmp_child_data(1); %set( dataH(1), 'visible', 'off' )
 end
-
 
 
 %% 5. select clusters
@@ -354,10 +376,13 @@ while repeat == 1
                         start_node = i;
                         way_time = sqrt((abs((nodes(start_node).x*PLAYGROUND_WIDTH)/PLAYGROUND_IMAGE_WIDTH-NODE_START_X_POSITION))^2 ...
                             +(abs((nodes(start_node).y*PLAYGROUND_HEIGHT)/PLAYGROUND_IMAGE_HEIGHT-NODE_START_Y_POSITION))^2)/ROBO_AVERAGE_SPEED;
-                        busy_node_time = nodes(start_node).time + way_time;
+                        busy_node_time = ceil(nodes(start_node).time + way_time);
                         set(nodes(i).child,'Color',MARKER_START_COLOR);
+                        set(nodes(i).weighttext,'Visible','off');
+                        text(nodes(i).x-5,nodes(i).y+5,num2str(node_count),'FontSize',20,'Color','w');
                         if nodes(start_node).cluster ~= 0
                             set(nodes(nodes(start_node).cluster).child,'Visible','off');
+                            set(nodes(nodes(start_node).cluster).weighttext,'Visible','off');
                             nodes(nodes(start_node).cluster).child = 0; 
                             node_count = node_count + 1; % count the completed nodes
                         end
@@ -373,6 +398,7 @@ while repeat == 1
     end
 end
 
+
 %% 5 start game
 % init variables
 strategy_track_enemy_grid = zeros(PLAYGROUND_HEIGHT/STRATEGY_TRACK_ENEMY_GRID_SIZE_Y, ...
@@ -384,6 +410,9 @@ ww2 = 0;
 
 % a loop with 90 iterations (one for every second)  
 for seconds = 1:PLAY_TIME
+    %
+    % enemy-tracking
+    %
     %set the position of the enemy robo
     disp('set next enemy robo position');
     figure(PLAYAREA);
@@ -394,40 +423,46 @@ for seconds = 1:PLAY_TIME
     imagesc(strategy_track_enemy_grid);
     colorbar;
     
+    %
+    % node-weighting
+    %
+    figure(PLAYAREA);
+    ww1 = (PLAY_TIME-seconds)/PLAY_TIME;
+    ww2 = (PLAY_TIME-(PLAY_TIME-seconds))/PLAY_TIME;
+    for i = 1:nodes_quantity
+        % destination node point-weight
+        if nodes(i).id == NODE_BALL_ID && nodes(i).x > nodes(start_node).x
+            w_dest = (nodes(i).points/nodes(i).time)*(1/nodes(i).percent);
+        else
+            w_dest = (nodes(i).points/nodes(i).time)*(1/nodes(i).percent)*2;
+        end
+        % destination node enemy-weight
+        w_enemy = strategy_track_enemy_grid(...
+        ceil(nodes(i).y/(PLAYGROUND_IMAGE_HEIGHT/(PLAYGROUND_HEIGHT/STRATEGY_TRACK_ENEMY_GRID_SIZE_Y))),...
+        ceil(nodes(i).x/(PLAYGROUND_IMAGE_WIDTH/(PLAYGROUND_WIDTH/STRATEGY_TRACK_ENEMY_GRID_SIZE_X))));
+        nodes(i).weight = ww1 * w_dest + ww1 * w_enemy;
+
+        % source -> destination node distance-time weight
+        w_src_dest = sqrt((abs((nodes(start_node).x*PLAYGROUND_WIDTH)/PLAYGROUND_IMAGE_WIDTH-(nodes(i).x*PLAYGROUND_WIDTH)/PLAYGROUND_IMAGE_WIDTH))^2 ...
+         +(abs((nodes(start_node).y*PLAYGROUND_HEIGHT)/PLAYGROUND_IMAGE_HEIGHT-(nodes(i).y*PLAYGROUND_HEIGHT)/PLAYGROUND_IMAGE_HEIGHT))^2)/ROBO_AVERAGE_SPEED;
+
+        node_edge_weight(start_node,i) = ww1 * w_dest + ww1 * w_enemy + ww2 * w_src_dest;
+        weight_history(i,seconds) = ww1 * w_dest + ww1 * w_enemy + w_src_dest;
+        
+        % plot weight informations            
+        set(nodes(i).weighttext,'String',num2str(node_edge_weight(start_node,i)));
+    end
+       
+    
+    %
+    % next-node searching
+    %
     if busy_node_time <= 0
     
         next_node.weight = inf;%node_edge_weight(start_node,1);
 
         for i = 1:nodes_quantity
             if nodes(i).child ~= 0 && i ~= start_node
-%                 nodes(i).weight = node_weight(seconds,strategy_track_enemy_grid,nodes(i),...
-%                     PLAYGROUND_IMAGE_WIDTH,PLAYGROUND_IMAGE_HEIGHT,PLAYGROUND_WIDTH,PLAYGROUND_HEIGHT,...
-%                     STRATEGY_TRACK_ENEMY_GRID_SIZE_X,STRATEGY_TRACK_ENEMY_GRID_SIZE_Y)
-% 
-%                 node_edge_weight(start_node,i) = (sqrt((abs((nodes(start_node).x*PLAYGROUND_WIDTH)/PLAYGROUND_IMAGE_WIDTH-(nodes(i).x*PLAYGROUND_WIDTH)/PLAYGROUND_IMAGE_WIDTH))^2 ...
-%                     +(abs((nodes(start_node).y*PLAYGROUND_HEIGHT)/PLAYGROUND_IMAGE_HEIGHT-(nodes(i).y*PLAYGROUND_HEIGHT)/PLAYGROUND_IMAGE_HEIGHT))^2))/ROBO_AVERAGE_SPEED+...
-%                     nodes(i).weight
-                
-%                 node_edge_weight(start_node,i) = node_weight(PLAY_TIME-seconds,PLAY_TIME,strategy_track_enemy_grid,nodes(start_node),nodes(i),...
-%                     PLAYGROUND_IMAGE_WIDTH,PLAYGROUND_IMAGE_HEIGHT,PLAYGROUND_WIDTH,PLAYGROUND_HEIGHT,...
-%                     STRATEGY_TRACK_ENEMY_GRID_SIZE_X,STRATEGY_TRACK_ENEMY_GRID_SIZE_Y);
-                
-                
-                ww1 = (PLAY_TIME-seconds)/PLAY_TIME;
-                ww2 = (PLAY_TIME-(PLAY_TIME-seconds))/PLAY_TIME;
-
-                w_dest = (nodes(i).points/nodes(i).time)*(1/nodes(i).percent);
-                
-                w_enemy = strategy_track_enemy_grid(...
-                    ceil(nodes(i).y/(PLAYGROUND_IMAGE_HEIGHT/(PLAYGROUND_HEIGHT/STRATEGY_TRACK_ENEMY_GRID_SIZE_Y))),...
-                    ceil(nodes(i).x/(PLAYGROUND_IMAGE_WIDTH/(PLAYGROUND_WIDTH/STRATEGY_TRACK_ENEMY_GRID_SIZE_X))));
-                nodes(i).weight = w_dest;
-
-                w_src_dest = sqrt((abs((nodes(start_node).x*PLAYGROUND_WIDTH)/PLAYGROUND_IMAGE_WIDTH-(nodes(i).x*PLAYGROUND_WIDTH)/PLAYGROUND_IMAGE_WIDTH))^2 ...
-                     +(abs((nodes(start_node).y*PLAYGROUND_HEIGHT)/PLAYGROUND_IMAGE_HEIGHT-(nodes(i).y*PLAYGROUND_HEIGHT)/PLAYGROUND_IMAGE_HEIGHT))^2);
-                
-                node_edge_weight(start_node,i) = ww1 * w_dest + ww2 * w_enemy + w_src_dest;
-                
                 % find the best node (lowest weight)
                 if next_node.weight > node_edge_weight(start_node,i)
                     next_node.node = i;
@@ -437,27 +472,29 @@ for seconds = 1:PLAY_TIME
             end
         end
 
-        %
+        node_count = node_count + 1; % count the completed nodes
+        
         % plot informations in playground
-        %
         figure(PLAYAREA);
         % draw route
         plot([nodes(start_node).x nodes(next_node.node).x],[nodes(start_node).y nodes(next_node.node).y],LINE_TREE_COLOR,'LineWidth',2);
         
         % disable the last and now completed node
         set(nodes(start_node).child,'Visible','off');
+        set(nodes(start_node).weighttext,'Visible','off');
         nodes(start_node).child = 0;
         % mark the new start node and disable his cluster pair
         start_node = next_node.node;
         set(nodes(next_node.node).child,'Color',MARKER_START_COLOR);
+        text(nodes(next_node.node).x-5,nodes(next_node.node).y+5,num2str(node_count),'FontSize',20,'Color','w');
         if nodes(next_node.node).cluster ~= 0
             set(nodes(nodes(next_node.node).cluster).child,'Visible','off');
+            set(nodes(nodes(next_node.node).cluster).weighttext,'Visible','off');
             nodes(nodes(next_node.node).cluster).child = 0; 
             node_count = node_count + 1; % count the completed nodes
         end
-        busy_node_time = nodes(start_node).time + next_node.time; %reset the busy-timer
+        busy_node_time = ceil(nodes(start_node).time + next_node.time); %reset the busy-timer
         
-        node_count = node_count + 1; % count the completed nodes
         
         % check if still incomplete nodes avaiable
         if node_count == nodes_quantity;
@@ -475,14 +512,14 @@ for seconds = 1:PLAY_TIME
         '; Zeit: ',num2str(seconds),'s; Knotenzeit: ',num2str(busy_node_time),'s'));
     
     figure(NODEWEIGHT);
-    subplot(222);
+    subplot(212);
     plot(seconds,ww1,'ro');
     plot(seconds,ww2,'bo');
     
-    subplot(221);
-    plot(seconds,nodes(1).weight*ww1,'o'); %ball node
-    plot(seconds,nodes(7).weight*ww1,'x'); %fresco node
-    plot(seconds,nodes(9).weight*ww1,'-'); %fire node  
+    subplot(211);
+    weight_handle = plot(seconds,weight_history(:,seconds),'o','LineWidth',2);
+    legend(weight_handle,'Ball 1','Ball 2','Ball 3','Ball 4','Ball 5','Ball 6','Fresco 1','Fresco 2',...
+        'Fire 1','Fire 2','Fire 3');
 end
 
 
